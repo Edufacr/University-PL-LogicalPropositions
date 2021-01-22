26c72a859aa7b254699bb3ac1342993a58aee662
Eduardo <edumadrim@gmail.com>
2021-01-22 14:29:49 -0600

    Vars foo

--- src/Vars.hs
+++ src/Vars.hs
@@ -1,18 +1,40 @@
 module Vars(vars)
 where
-    
+
 import Proposition(Proposition(..))
+
 
 vars :: Proposition -> [String]
 
-vars (Variable var) = [var]
-vars (Negation prop) = vars prop
--- ? Debe haber una forma de poner estas en una sola 
-vars (Conjuction prop1 prop2) = vars prop1 ++ vars prop2
-vars (Disjunction prop1 prop2) = vars prop1 ++ vars prop2
-vars (Conditional prop1 prop2) = vars prop1 ++ vars prop2
-vars (Biconditional prop1 prop2) = vars prop1 ++ vars prop2
+vars p = nub(vars' p)
+    where 
+        vars' (Variable var)                = [var]
+        vars' (Negation prop)               = vars' prop
+        vars' (Conjuction prop1 prop2)      = vars' prop1 ++ vars' prop2
+        vars' (Disjunction prop1 prop2)     = vars' prop1 ++ vars' prop2
+        vars' (Conditional prop1 prop2)     = vars' prop1 ++ vars' prop2
+        vars' (Biconditional prop1 prop2)   = vars' prop1 ++ vars' prop2
 
-rmDupli :: [*] -> [*]
-rmDupli [] = []
---rmDupli (x:xs) =  
+----------------------------------------------
+-- Based on Nub from Data.List
+-- Author: The University of Glasgow 2001
+----------------------------------------------
+nub :: (Eq e) => [e] -> [e]
+
+nub list = nub' list []
+    where 
+        nub' [] _                               = []
+        nub' (first:left_of_list) foundElements
+            | foundElements `contains` first    = nub' left_of_list foundElements
+            | otherwise                         = first : nub' left_of_list (first:foundElements)
+
+contains:: (Eq e) => [e] -> e -> Bool
+
+contains [] _ = False 
+contains (x:xs) e
+    | x == e = True
+    | otherwise = contains xs e
+
+
+
+
