9f05f63887739ba826d4f4be7dec05b218efb568
Eduardo <edumadrim@gmail.com>
2021-01-24 20:10:35 -0600

    lenght condition deleted & new zip foo

--- src/As_Vals.hs
+++ src/As_Vals.hs
@@ -5,11 +5,11 @@ import Proposition(Proposition(..))
 
 asVals:: [String] -> [Bool] -> [(String, Bool)]
 
-asVals pList bList 
-    | length pList == length bList = zip1 pList bList
-    | otherwise = []
+asVals = zipVarVals
 
-zip1:: [a] -> [b] -> [(a,b)]
+-- Checks if bool list lenght >= var list lenght
+zipVarVals:: [String] -> [Bool] -> [(String,Bool)]
 
-zip1 [] [] = []
-zip1 (x:xs) (y:ys) = (x,y) : zip1 xs ys
+zipVarVals (x:xs) (y:ys) = (x,y) : zipVarVals xs ys
+zipVarVals [] _ = []
+zipVarVals _ _ = error "Not enough values given"
